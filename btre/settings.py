43f1f7eb6373b8576ff280a0435869ef5036eae2
David Robb <66834033+drobb2020@users.noreply.github.com>
2021-08-19 14:27:05 -0400

    Contact submission and email setup

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email Configuration
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = '587'
+# EMAIL_HOST_USER = 'drobb2011@gmail.com'
+# EMAIL_HOST_PASSWORD = 'zB6nJNQ?t@!8YNM#'
+# EMAIL_USE_TLS = True
